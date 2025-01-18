b7be4eb6b51771b32c53522f6941196f930bbf5c
LorenzSchrader <114871756+LorenzSchrader@users.noreply.github.com>
2025-01-18 17:21:25 +0100

    Update tracking.js, adding an Event listener, that executes the page view tracking after the DOM Content is loaded

--- tracking.js
+++ tracking.js
@@ -1,9 +1,11 @@
 // Scripts to track the User Behaviour:
 
 // 1) Tack the Page View. This allows to identify new users by allocating a specific ID.
-rudderanalytics.page();
-// We log the event to the console, to ensure that the event is successfully tracked.
-console.log("Page View Tracked");
+document.addEventListener('DOMContentLoaded', function() {
+  // Track the Page View
+  rudderanalytics.page("User", "Sign Up");
+  console.log("Page View Tracked");
+
 
 // 2) Track the User Behaviour Events (in this case the Signup Event).
 document.getElementById("btn-signup").addEventListener("click", function () {
